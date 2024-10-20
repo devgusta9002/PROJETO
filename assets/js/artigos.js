123bfabec1fc2ab66b24f88fe832b66240525ece
devgusta9002 <portelagustavo947@gmail.com>
2024-10-20 13:09:40 -0300

    .

--- assets/js/artigos.js
+++
@@ -1,9 +0,0 @@
-
-function img(anything) {
-    document.querySelector('.slide').src = anything;
-  }
-
-  function change(change) {
-    const line = document.querySelector('.home');
-    line.style.background = change;
-  }
